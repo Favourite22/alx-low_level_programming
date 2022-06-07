fb6f25bcc4931b88516cc88299fed663f3fd2c2d
Favourite22 <enriquefavour@gmail.com>
2022-06-06 19:12:14 -0700

    SB

--- 5-print_numbers.c
+++ 5-print_numbers.c
@@ -2,13 +2,9 @@
 
 /**
  * main - program prints all single digit numbers of base 10 starting from 0
- *
  * All your code should be in the main function
- *
  * Return: 0
  */
-
-
 
 int main(void)
 
